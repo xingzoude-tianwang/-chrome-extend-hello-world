2fa472ae8556f4ab8412938827fbf5880b1cc8eb
xingzoudetianwang@gmail.com <xingzoudetianwang@gmail.com>
2021-04-01 23:17:18 +0800

    read doc about browser_action

--- dist/scripts/background.js
+++ dist/scripts/background.js
@@ -1,1 +1,46 @@
-console.log('background.js');
+function testToggle(tab) {    
+    const keys = Object.keys(tab)
+    const text = JSON.stringify(tab)
+    chrome.browserAction.setBadgeText({
+        text: keys[1]
+    })
+    chrome.browserAction.setBadgeBackgroundColor({
+        color: [255, 0, 0, 255]
+    })
+    chrome.browserAction.setIcon({
+        path: '../asset/img/icon2.png'
+    })
+    chrome.browserAction.setPopup({
+        popup: '../page/bye.html'
+    })
+    chrome.browserAction.setTitle({
+        title: text
+    })
+    const contextMenusParam = {
+        type: "radio", //"normal", "checkbox", "radio", "separator"
+        title: "tttt",
+        checked: false,
+        onclick(info, tab) {
+            chrome.browserAction.setTitle({
+                title: JSON.stringify(info) + tab,
+            }) 
+            updateContextMenus(info.menuItemId);
+        },
+        contexts: ["page"] //["all", "page", "frame", "selection", "link", "editable", "image", "video", "audio"] 
+    };
+    chrome.contextMenus.create(contextMenusParam);
+    updateContextMenus = (id) => {
+        const contextMenusUpdateParam = {
+            id: `${id}`,
+            checked: true,
+            title: "its true",
+            type: "radio",
+        }
+        chrome.contextMenus.create(contextMenusUpdateParam);
+    }
+}
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//     testToggle(tab);
+//     console.log(tab);
+// });
+testToggle({ tab: 'test' });
